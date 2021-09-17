fef94ea4eeb74f2ab33470a15674869db8cef64b
Arden Johannes Sasan <ajsasan@dnamicro.com>
2021-09-17 11:36:04 +0800

    removed unused files

---
+++ src/SolarSystem/components/Orbit/index.tsx
@@ -0,0 +1,10 @@
+import { FC } from "react";
+import { Props } from "./types";
+
+const Orbit:FC<Props> = () =>{
+    return <mesh>
+        <ringGeometry />
+    </mesh>
+}
+
+export default Orbit;
